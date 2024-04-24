d7fd630b1b76ab0162235c72423ab151601109db
bigdave7 <yuzhoudu2023@u.northwestern.edu>
2024-04-23 23:32:21 -0400

    In Progress

--- module5/js/script.js
+++ module5/js/script.js
@@ -10,9 +10,9 @@ $(function () {
 (function (global) {
     var dc = {};
     var homeHtml = "https://bigdave7.github.io/coursera-test/module5/snippets/home-snippet.html";
-    var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
-    var categoriesTitleHtml = "";
-    var categoryHtml = "";
+    var allCategoriesUrl = "";
+    var categoriesTitleHtml = "https://bigdave7.github.io/coursera-test/module5/snippets/categories-title-snippet.html";
+    var categoryHtml = "https://bigdave7.github.io/coursera-test/module5/snippets/category-snippet.html";
 
     var insertHtml = function (selector, html) {
         var targetElem = document.querySelector(selector);
@@ -25,16 +25,48 @@ $(function () {
         insertHtml(selector, html);
     };
 
+    var insertProperty = function (string, propName, propValue) {
+        var propToReplace = "{{" + propName + "}}";
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
+    }
+
     document.addEventListener("DOMContentLoaded", function (event) {
         showLoading("#main-content");
-        $.ajax({
-            url: homeHtml,
-            type: 'GET',
-            success: function(responseText) {
-              document.querySelector("#main-content").innerHTML = responseText;
-            }
-          });
+        $.get(homeHtml, function (responseText) {
+            document.querySelector("#main-content").innerHTML = responseText;
+        }, "html");
     });
+
+    dc.loadMenuCategories = function () {
+        showLoading("#main-content");
+        $.get(allCategoriesUrl, buildAndShowCategoriesHTML, "json");
+    }
+
+    function buildAndShowCategoriesHTML (categories) {
+        $.get(categoriesTitleHtml, function (categoriesTitleHtml) {
+            $.get(categoryHtml, function (categoryHtml) {
+                var categoriesViewHtml = buildAndShowCategoriesHTML(categories, categoriesTitleHtml, categoryHtml);
+                insertHtml("#main-content", categoriesViewHtml);
+            }, "html");
+        }, "html");
+    }
+
+    function buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml) {
+        var finalHtml = categoriesTitleHtml;
+        finalHtml += "<section class='row'>";
+
+        for (var i = 0; i < categories.length; i++) {
+            var html = categoryHtml;
+            var name = "" + categories[i].name;
+            var short_name = categories[i].short_name;
+            html = insertProperty(html, "name", name);
+            html = insertProperty(html, "short_name", short_name);
+            finalHtml += html;
+        }
+        finalHtml += "</section>";
+        return finalHtml;
+    }
 
     global.$dc = dc;
 
